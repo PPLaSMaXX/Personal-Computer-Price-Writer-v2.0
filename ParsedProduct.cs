d8edc4d1a49db451688e66b6087fc97d2084e845
EneRgy_GOD <48410107+EneRgyGOD@users.noreply.github.com>
2021-03-03 03:50:32 +0200

    added class of parsed product

---
+++ ParsedProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPW2
+{
+    class ParsedProduct
+    {
+        ParsedProduct(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string name { get; }
+        public int price { get; }
+    }
+}
